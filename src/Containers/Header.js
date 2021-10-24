8a8706a6e8ea1cc2647cc10e7db76bd30f4b5a63
Justin <justinhwang92@gmail.com>
2021-10-24 16:26:28 -0400

    Add comments

--- src/Containers/Header.js
+++ src/Containers/Header.js
@@ -1,6 +1,9 @@
 import React from "react";
+// Components
 import { Header } from "../components/Header";
+// Routes
 import * as ROUTES from "../Constants/routes";
+// Logo
 import Logo from "../logo.svg";
 
 export const HeaderContainer = ({ children }) => {
