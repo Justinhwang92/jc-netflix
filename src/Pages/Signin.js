93aeaca539702a8d0e6118a9d02e76c431675f57
Justin <justinhwang92@gmail.com>
2021-10-27 16:03:34 -0400

    Implement Signin

--- src/Pages/Signin.js
+++ src/Pages/Signin.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { FooterContainer } from "../Containers/Footer";
+import { HeaderContainer } from "../Containers/Header";
+import { Form } from "../components/Form";
 
 const Signin = () => {
-  return <p>Hello from Signin</p>;
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  // check from input elements are valid
+  // email & password
+  const handleSignin = (event) => {
+    event.preventDefault();
+
+    // firebase work here
+  };
+  const isInvalid = password === "" || emailAddress === "";
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignin} method="POST">
+            <Form.Input
+              placeholder="Email Address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              autoComplete="off"
+              placeholder="Password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+
+          <Form.Text>
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. Learn more.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      ;
+      <FooterContainer />
+    </>
+  );
 };
 
 export default Signin;
