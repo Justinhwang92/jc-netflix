faf9d7bb8c46775b47b1edbe73259afffe403211
Justin <justinhwang92@gmail.com>
2021-12-21 14:39:31 -0500

    Add use-auth-listener hook

--- src/App.js
+++ src/App.js
@@ -1,44 +1,48 @@
 import React from "react";
 // Routes
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import * as ROUTES from "./Constants/routes";
 // Pages
 import { Home, Browse, Signin, Signup } from "./Pages";
 // Helpers
 import { IsUserRedirect, ProtectedRoute } from "./Helpers/Route";
+// Hooks
+import { useAuthListener } from "./Hooks";
 
 export default function App() {
-  const user = null;
+  const user = useAuthListener();
 
   return (
     <Router>
-      <IsUserRedirect
-        user={user}
-        loggedInPath={ROUTES.BROWSE}
-        path={ROUTES.SIGN_IN}
-        exact
-      >
-        <Signin />
-      </IsUserRedirect>
-      <IsUserRedirect
-        user={user}
-        loggedInPath={ROUTES.BROWSE}
-        path={ROUTES.SIGN_UP}
-        exact
-      >
-        <Signup />
-      </IsUserRedirect>
-      <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
-        <Browse />
-      </ProtectedRoute>
-      <IsUserRedirect
-        user={user}
-        loggedInPath={ROUTES.BROWSE}
-        path={ROUTES.HOME}
-        exaxt
-      >
-        <Home />
-      </IsUserRedirect>
+      <Switch>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_IN}
+          exact
+        >
+          <Signin />
+        </IsUserRedirect>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_UP}
+          exact
+        >
+          <Signup />
+        </IsUserRedirect>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.HOME}
+          exaxt
+        >
+          <Home />
+        </IsUserRedirect>
+      </Switch>
     </Router>
   );
 }
