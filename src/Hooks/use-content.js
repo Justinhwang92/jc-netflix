2e6748536152d0861c34f5ecb119b0388aa07fd6
Justin <justinhwang92@gmail.com>
2021-12-21 14:51:36 -0500

    Add useContent hook

---
+++ src/Hooks/use-content.js
@@ -0,0 +1,28 @@
+import { useState, useEffect, useContext } from "react";
+import { FirebaseContext } from "../Context/Firebase";
+
+export default function useContent(target) {
+  const [content, setContent] = useState([]);
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    // Access the target collection
+    firebase
+      .firestore()
+      .collection(target)
+      .get()
+      .then((snapshot) => {
+        const allContent = snapshot.docs.map((contentObj) => ({
+          ...contentObj.data(),
+          docID: contentObj.id, // use docID for unique key
+        }));
+
+        setContent(allContent);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }, []);
+
+  return { [target]: content };
+}
--- src/Pages/Browse.js
+++ src/Pages/Browse.js
@@ -1,6 +1,16 @@
 import React from "react";
+// Hooks
+import { useContent } from "../Hooks";
 
 const Browse = () => {
+  // we need the series and the filims
+  const { series } = useContent("series");
+  const { films } = useContent("films");
+  console.log(series);
+  console.log(films);
+  // we need slides
+  // pass it to the brows container
+
   return <p>Hello from Browse</p>;
 };
 
