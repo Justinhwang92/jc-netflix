19f9b0d89f77622b7f0445b0c9882f7689681aba
Justin <justinhwang92@gmail.com>
2022-01-10 17:40:09 -0500

    Add functions in Header profile

--- src/Containers/Browse.js
+++ src/Containers/Browse.js
@@ -3,7 +3,11 @@ import { SelectProfileContainer } from "./Profiles";
 // Context
 import { FirebaseContext } from "../Context/Firebase";
 // Components
-import { Loading } from "../components";
+import { Header, Loading } from "../components";
+// Routes
+import * as ROUTES from "../Constants/routes";
+// Logo
+import logo from "../logo.svg";
 
 export function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({});
@@ -18,11 +22,41 @@ export function BrowseContainer({ slides }) {
   }, [profile.displayName]);
 
   return profile.displayName ? (
-    loading ? (
-      <Loading src={user.photoURL} />
-    ) : (
-      <Loading.ReleaseBody />
-    )
+    <>
+      {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+      <Header src="joker1" dontShowOnSmallViewPort>
+        <Header.Frame>
+          <Header.Group>
+            <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+            <Header.TextLink>Series</Header.TextLink>
+            <Header.TextLink>Films</Header.TextLink>
+          </Header.Group>
+
+          <Header.Group>
+            <Header.Profile>
+              <Header.Picture src={user.photoURL} />
+              <Header.Dropdown>
+                <Header.Group>
+                  <Header.Picture src={user.photoURL} />
+                  <Header.TextLink>{user.displayName}</Header.TextLink>
+                </Header.Group>
+              </Header.Dropdown>
+            </Header.Profile>
+          </Header.Group>
+        </Header.Frame>
+
+        <Header.Feature>
+          <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+          <Header.Text>
+            Forever alone in a crowd, failed comedian Arthur Fleck seeks
+            connection as he walks the streets of Gotham City. Arthur wears two
+            masks -- the one he paints for his day job as a clown, and the guise
+            he projects in a futile attempt to feel like he's part of the world
+            around him.
+          </Header.Text>
+        </Header.Feature>
+      </Header>
+    </>
   ) : (
     <SelectProfileContainer user={user} setProfile={setProfile} />
   );
