aa84c9f94fb615f2843c449a2db18b245904c8c2
Justin <justinhwang92@gmail.com>
2022-01-13 13:51:31 -0500

    Change firebase config to env

--- src/Lib/Firebase.prod.js
+++ src/Lib/Firebase.prod.js
@@ -1,9 +1,19 @@
 import Firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
-import { firebaseConfig } from "./Firebase.config";
+// import { firebaseConfig } from "./Firebase.config";
 // One time execution only
 // import { seedDatabase } from "../Seed";
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
 
 const firebase = Firebase.initializeApp(firebaseConfig);
 
